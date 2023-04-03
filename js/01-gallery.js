663f2ac90e9c93d3f307cdfcd366b83b68aaa9fd
Mariia2811 <mary.stupka11@gmail.com>
2023-04-03 17:47:16 +0300

    edit

---
+++ js/01-gallery.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const refListEl = document.querySelector('.gallery');
+
+const imgMarkUp = galleryItems.map(({preview, original, description}) => 
+    `<li class="gallery__item"> 
+      <a class="gallery_link" href="${original}">
+      <img src="${preview}" data-sourse="${original}" alt="${description}" class="gallery__image">
+      </a>
+    </li>`).join('');
+
+refListEl.insertAdjacentHTML('beforeend', imgMarkUp);
+
+refListEl.addEventListener('click', (event) => {
+  event.preventDefault();
+  const refImgEl = event.target.classList.contains('gallery__image');
+  if (!refImgEl) {
+    return
+  }
+  const imgElSrc = event.target.dataset.sourse;
+  const instense = basicLightbox.create (`
+  <img width="1400" height="900" src="${imgElSrc}">
+  `).show()
+});
+
+console.log(galleryItems);
